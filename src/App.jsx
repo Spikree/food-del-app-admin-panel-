38963a9fb19ec856fb7f05314f90a099496647b0
Spikree <avimahalingpure10@gmail.com>
2024-05-06 22:00:53 +0530

    added features

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Add from './pages/add/Add'
 import List from './pages/list/List'
 import Orders from './pages/orders/Orders'
 import {Route, Routes} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return <>
     <div>
+      <ToastContainer/>
       <Navbar/>
       <hr />
       <div className="app-content">
--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -1,9 +1,13 @@
 import './add.css'
 import {assets} from '../../assets/assets'
 import { useState } from 'react'
+import axios from "axios"
+import { toast } from 'react-toastify'
 
 const Add = () => {
 
+
+  const url = "http://localhost:4000";
   const [image,setImage] = useState(false);
   const [data,setDate] = useState({
     name:"",
@@ -12,9 +16,38 @@ const Add = () => {
     category:"Salad"
   });
 
+  const onChangeHandler = (event) => {
+   const name = event.target.name;
+   const value = event.target.value;
+   setDate(data=>({...data,[name]:value}))
+  }
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name",data.name)
+    formData.append("description",data.description)
+    formData.append("price",Number(data.price))
+    formData.append("category",data.category)
+    formData.append("image",image)
+    const response = await axios.post(`${url}/api/food/add`,formData); // sends the form data
+    if(response.data.success) {
+      setDate({
+        name:"",
+        description:"",
+        price:"",
+        category:"Salad"
+      })
+      setImage(false)
+      toast.success(response.data.message);
+    } else {
+      toast.error(response.data.message)
+    }
+  }
+
   return (
     <div className='add'>
-      <form className='flex-col'>
+      <form className='flex-col' onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>Upload image</p>
           <label htmlFor="image">
@@ -25,18 +58,18 @@ const Add = () => {
 
         <div className="add-product-name flex-col">
             <p>Product name</p>
-            <input type="text" name='name' placeholder='Type here' />
+            <input onChange={onChangeHandler} value={data.name} type="text" name='name' placeholder='Type here' />
         </div>
 
         <div className="add-product-description flex-col">
           <p>Product description</p>
-          <textarea name="description" rows="6" placeholder='Write content here' required></textarea>
+          <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6" placeholder='Write content here' required></textarea>
         </div>
 
         <div className="add-category-price">
           <div className="add-category flex-col">
               <p>Product category</p>
-              <select name="category">
+              <select onChange={onChangeHandler} name="category">
                 <option value="Salad">Salad</option>
                 <option value="Rolls">Rolls</option>
                 <option value="Desert">Desert</option>
@@ -50,7 +83,7 @@ const Add = () => {
 
           <div className="add-price flex-col">
               <p>Product Price</p>
-              <input type="Number" name="price" placeholder='$20' />
+              <input onChange={onChangeHandler} value={data.price} type="Number" name="price" placeholder='$20' />
           </div>
         </div>
         <button type='submit' className='add-btn'>Add</button>
