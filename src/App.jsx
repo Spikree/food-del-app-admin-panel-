620525c7ee328701c6630c5a780847c93788b1f0
Avi <avimahalingpure10@gmail.com>
2024-06-29 19:07:07 +0530

    order page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
-  const url = "http://localhost:4000"
+  const url = "http://localhost:5001"
 
   return <>
     <div>
--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,8 +1,56 @@
+import { useState } from 'react'
 import './orders.css'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useEffect } from 'react'
+import { assets } from '../../assets/assets.js'
 
-const Orders = () => {
+const Orders = ({url}) => {
+
+  const [ orders,setOrders ] = useState([])
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url+'/api/order/list');
+    if(response.data.success) {
+      setOrders(response.data.data)
+    } else {
+      toast.error("Error")
+    }
+  }
+
+  useEffect(() => {
+    fetchAllOrders();
+  },[])
+
   return (
-    <div>Orders</div>
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {
+          orders.map((order,index) => {
+            (
+              <div key={index} className="order-item">
+                <img src={assets.parcel_icon} alt="" />
+                <div>
+                  <p className='order-item-food'>
+                    {order.items.map((item,index) => {
+                      if(index== order.items.length - 1) {
+                        return item.name + " x " + item.quantity
+                      } else {
+                        return item.name + " x " + item.quantity + ", "
+                      }
+                    })}
+                  </p>
+                  <p className="order-item-name">
+                    {order.address.firstName + " " + order.address.lastName}
+                  </p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
